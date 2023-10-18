e1a47110bd519b572fb93ba17b9c9fcef652c571
ravurigovind111@gmail.com <ravurigovind111@gmail.com>
2023-10-18 09:30:52 -0500

    Initial commitcvcc

--- src/main/java/com/example/mainProject/controller/BookingController.java
+++ src/main/java/com/example/mainProject/controller/BookingController.java
@@ -7,12 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@CrossOrigin(origins="http://localhost:4200")
 public class BookingController {
    @Autowired
    private BookingService bookingService;
